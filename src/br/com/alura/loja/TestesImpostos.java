3acc124221e6c2bd3374dd3b92f2e555aad96f87
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-09 14:49:51 -0300

    Adicionando design pattern decorator

--- src/br/com/alura/loja/TestesImpostos.java
+++ src/br/com/alura/loja/TestesImpostos.java
@@ -10,14 +10,13 @@ import br.com.alura.loja.orcamento.Orcamento;
 public class TestesImpostos {
 
 	public static void main(String[] args) {
-		
-		Orcamento orcamento= new Orcamento(new BigDecimal("100"), 1);
-		CalculadoraDeImpostos calc = new CalculadoraDeImpostos();
-		
-		System.out.println(calc.calcular(orcamento, new ICMS()));
-		System.out.println(calc.calcular(orcamento, new ISS()));
 
-		
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+		CalculadoraDeImpostos calc = new CalculadoraDeImpostos();
+
+		System.out.println(calc.calcular(orcamento, new ICMS(null)));
+		System.out.println(calc.calcular(orcamento, new ISS(null)));
+		System.out.println(calc.calcular(orcamento, new ICMS(new ISS(null))));
 
 	}
 
