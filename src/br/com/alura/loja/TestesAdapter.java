94677ab70455f35653f7b3cf60e763424fb9cc03
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-12 08:24:35 -0300

    Adicionando o design pattern composite

--- src/br/com/alura/loja/TestesAdapter.java
+++ src/br/com/alura/loja/TestesAdapter.java
@@ -3,6 +3,7 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.http.JavaHttpClient;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.orcamento.RegistroDeOrcamento;
 
@@ -10,7 +11,8 @@ public class TestesAdapter {
 
 	public static void main(String[] args) {
 
-		Orcamento orcamento = new Orcamento(new BigDecimal("10"), 1);
+		Orcamento orcamento = new Orcamento();
+		orcamento.adicionarItem(new ItemOrcamento(new BigDecimal("10")));
 		orcamento.aprovar();
 		orcamento.finalizar();
 
--- src/br/com/alura/loja/TestesDescontos.java
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -3,15 +3,19 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class TestesDescontos {
 
 	public static void main(String[] args) {
 
-		Orcamento orcamento1 = new Orcamento(new BigDecimal("200"), 6);
-		Orcamento orcamento2 = new Orcamento(new BigDecimal("1000"), 1);
-		
+		Orcamento orcamento1 = new Orcamento();
+		orcamento1.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
+
+		Orcamento orcamento2 = new Orcamento();
+		orcamento2.adicionarItem(new ItemOrcamento(new BigDecimal("1000")));
+
 		CalculadoraDeDescontos calc = new CalculadoraDeDescontos();
 
 		System.out.println(calc.calcular(orcamento1));
